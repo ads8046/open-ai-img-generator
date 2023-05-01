d68c27bae3a27781a01e9ee467b16076bb902c5b
ads8046 <ads8046@rit.edu>
2023-05-01 03:58:37 -0400

    OpenAI basic backend setup complete

--- src/App.js
+++ src/App.js
@@ -1,3 +1,7 @@
+import {useState} from "react";
+
+
+
 
 const App = () => {
   // const surpriseOptions = [
@@ -5,26 +9,51 @@ const App = () => {
   //     'An ant drinking apple juice',
   //     'A pineapple drinking lemonade on a beach'
   // ]
+    const [images, setImages] = useState(null);
+    const [value, setValue] = useState(null);
 
-  return (
-    <div className="App">
+    const getImages = async () => {
+        const options = {
+            method: "POST",
+            body: JSON.stringify({
+                message: "test"
+            }),
+            headers: {
+                "Content-type": "application/json"
+            }
+        }
+        try {
+            const resp = await fetch("http://localhost:8000/images", options);
+            const data = await resp.json();
+            setImages(data);
+        } catch (error) {
+            console.log(error);
+        }
+    }
 
-      <section className={"search-section"}>
-        <p>Start with a detailed description
-          <span className={"surprise"}>Surprise me!</span>
-        </p>
-        <div className={"input-container"}>
-          <input
-              placeholder={"An impressionist oil painting of a sunflower in a violet vase..."}
-          />
-          <button>Generate</button>
+    return (
+        <div className="App">
+
+          <section className={"search-section"}>
+            <p>Start with a detailed description
+              <span className={"surprise"}>Surprise me!</span>
+            </p>
+            <div className={"input-container"}>
+              <input
+                  placeholder={"An impressionist oil painting of a sunflower in a violet vase..."}
+              />
+              <button onClick={getImages}>Generate</button>
+            </div>
+          </section>
+
+          <section className={"image-section"}>
+              {images?.map((img, _index) =>
+                    <img key={_index} src={img.url} alt={`Generated AI pic of ${value}`}/>
+              )}
+          </section>
+
         </div>
-      </section>
-
-      <section className={"image-section"}></section>
-
-    </div>
-  );
+    );
 }
 
 export default App;
